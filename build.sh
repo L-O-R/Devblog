c407e31739d77bcf863a76a52b0f85b1666eec7a
L-O-R <lokeshrathi37455@gmail.com>
2025-09-09 11:40:14 +0530

    update build.sh file to create super user

--- build.sh
+++ build.sh
@@ -9,6 +9,18 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # for the creating superuser..
-python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+import os;
+User = get_user_model();
+username=os.environ.get('DJANGO_SUPERUSER_USERNAME','admin');
+email=os.environ.get('DJANGO_SUPERUSER_EMAIL','admin@example.com');
+password=os.environ.get('DJANGO_SUPERUSER_PASSWORD','admin123');
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created ✅');
+else:
+    print('Superuser already exists');
+"
 # Run our database "construction" plan
 python manage.py migrate
